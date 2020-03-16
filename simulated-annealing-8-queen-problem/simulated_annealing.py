ecf16678b2474ddbd57239900be45cb18c092568
Sharif Nasser <sharifnasser@gmail.com>
2020-03-16 16:15:13 -0600

    Add algorithm

--- simulated-annealing-8-queen-problem/simulated_annealing.py
+++ simulated-annealing-8-queen-problem/simulated_annealing.py
@@ -1,3 +1,6 @@
+import random
+from math import exp
+
 board_size = 8
 
 def count_crossing_queens(positions):
@@ -33,9 +36,84 @@ estado_inicial = queens = [[i, random.randrange(board_size)] for i in range(boar
 
 intentos = 1
 u = estado_inicial
-f = count_attacking_queens(estado_inicial)
+evaluacion_u = count_attacking_queens(estado_inicial)
 
 mejor = u
 mejor_intentos = intentos
 
 c = 0.1
+
+def crear_vecino(queens):
+        row_selection = random.randrange(board_size)
+        queen_selection = queens[row_selection]
+        new_column = random.choice(list(range(0, queen_selection[1])) + list(range(queen_selection[1]+1, board_size)))
+        
+        neighbor = queens
+        neighbor[row_selection][1] = new_column
+        return neighbor
+
+def acepta_intento(u, v):
+        evaluacion_v = count_attacking_queens(v)
+        evaluacion_u = count_attacking_queens(u)
+        if evaluacion_v < count_attacking_queens(u):
+                return True
+        else:
+                return (random.random() < exp( -1 * (evaluacion_v - evaluacion_u) / c ))
+                
+
+def cadena_markov(u):
+        global c
+        intentos = 0
+        intentos_aceptados = 0
+        print(u)
+        while (intentos_aceptados < cadena_intentos_aceptados) and (intentos < cadena_intentos_maximo):
+                v = crear_vecino(u)
+                evaluacion_v = count_attacking_queens(v)
+
+                intentos += 1
+                if acepta_intento(u, v):
+                        c = c * (evaluacion_v / evaluacion_u)
+
+                        u = v
+                        intentos_aceptados += 1
+
+        razon_aceptacion = 1.0 * (intentos_aceptados / intentos)
+        u_final = u
+
+        return u_final, razon_aceptacion
+
+def calcular_temperatura_inicial(u):
+        global c
+        razon_aceptacion = 0
+        while razon_aceptacion < minimo_razon_aceptados:
+                [u, razon_aceptacion] = cadena_markov(u)
+                c = c * beta
+
+        u_inicial = u
+
+        return u_inicial
+
+def recocido(u):
+        global c
+        cadena_sin_mejora = 0
+        anterior = u
+        while (cadena_sin_mejora < cadena_maximo_sin_mejora):
+                u, razon_aceptacion = cadena_markov(u)
+                evaluacion_anterior = count_attacking_queens(anterior)
+                evaluacion_u = count_attacking_queens(u)
+
+                if evaluacion_u >= evaluacion_anterior:
+                        cadena_sin_mejora += 1
+                else:
+                        cadena_sin_mejora = 0
+
+                anterior = u
+                c = c * alfa
+
+        return u
+
+u = calcular_temperatura_inicial(estado_inicial)
+print('temp incial', c)
+
+print(recocido(u))
+print(count_attacking_queens(u))
