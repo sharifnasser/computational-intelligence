20148d4556e57f281367f0b553ea3b21f9e6958d
Sharif Nasser <sharifnasser@gmail.com>
2020-04-26 20:05:05 -0500

    Add weights and gains of knapsack problem

--- particle-swarm-knapsack-problem/particle-swarm.py
+++ particle-swarm-knapsack-problem/particle-swarm.py
@@ -6,7 +6,13 @@ import matplotlib.pyplot as plt
 from copy import deepcopy
 from math import exp
 
-swarm_size = 1
-swarm = np.array([np.random.randint(2, size=10) for i in range(swarm_size)])
+number_of_objects = 10
+swarm_size = 3
+swarm = np.array([np.random.randint(2, size=number_of_objects) for i in range(swarm_size)])
+weights = np.array([23, 31, 29, 44, 53, 38, 63, 85, 89, 82])
+gains = np.array([92, 57, 49, 68, 60, 43, 67, 84, 87, 72])
 
 print(swarm)
+
+for particle in swarm:
+    print(particle)
