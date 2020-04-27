2638a163bc83a740fd986ab271f54ac4f8330699
Sharif Nasser <sharifnasser@gmail.com>
2020-04-26 23:34:40 -0500

    Add comments

--- binary-particle-swarm-knapsack-problem/particle-swarm.py
+++ binary-particle-swarm-knapsack-problem/particle-swarm.py
@@ -4,11 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import exp
 
-# initialize parameters
+# initialize algorithm parameters
 max_velocity = 1 # maximum velocity for particles
 alpha = 2 # cognitive  scaling
 beta = 2 # social scaling
 swarm_size = 10 # number of particles in the swarm
+max_runs_no_improve = 100 # maximum number of runs without improvement
 
 # initialize knapsack problem
 number_of_objects = 10 # number of objects available
@@ -21,8 +22,7 @@ particle_best = np.array([np.zeros(number_of_objects, dtype=int) for i in range(
 swarm_best = np.zeros(number_of_objects, dtype=int) # initialize best particle in the swarm
 velocity = np.array([np.zeros(number_of_objects, dtype=int) for i in range(swarm_size)]) # initialize array with velocities of particles to zero
 
-
-max_runs_no_improve = 100 # maximum number of runs without improvement
+# initialize algorithm evaluation
 best_found_list = [] # save best combination found by number of particles movements
 len_algorithm_evaluation = 10 # number of algorithm evaluations
 best_found_algorithm_evaluation = [] # best found list for each algorithm evaluation
@@ -45,29 +45,36 @@ def limit_velocity(velocity):
 
 for algorithm_evaluation in range(len_algorithm_evaluation):
     start_time = time.time()
-
     runs_no_improve = 0
+
     while runs_no_improve < max_runs_no_improve:
         for particle in range(swarm_size):
             for bit in range(number_of_objects):
-                velocity[particle][bit] = velocity[particle][bit] + alpha * np.random.uniform(0, 1) * (particle_best[particle][bit] - swarm[particle][bit]) + beta * np.random.uniform(0, 1) * (swarm_best[bit] - swarm[particle][bit])
-                velocity[particle][bit] = limit_velocity(velocity[particle][bit])
-                swarm[particle][bit] = 1 if np.random.uniform(0, 1) < sigmoid(velocity[particle][bit]) else 0
+                # calculate velocity for each bit of every particle
+                velocity[particle][bit] = velocity[particle][bit] \
+                                        + alpha * np.random.uniform(0, 1) * (particle_best[particle][bit] - swarm[particle][bit]) \
+                                        + beta * np.random.uniform(0, 1) * (swarm_best[bit] - swarm[particle][bit])
 
-            best_found_list.append(evaluate_combination(swarm_best))
+                velocity[particle][bit] = limit_velocity(velocity[particle][bit]) # limit the velocity between +/- maximum velocity
+                
+                swarm[particle][bit] = 1 if np.random.uniform(0, 1) < sigmoid(velocity[particle][bit]) else 0 # select value of bit with sigmoid function
 
+            best_found_list.append(evaluate_combination(swarm_best)) # saved best found evaluation for every particle changed
+
+            # save particle as its best local if it has a better evaluation
             if evaluate_combination(swarm[particle]) > evaluate_combination(particle_best[particle]):
                 particle_best[particle] = swarm[particle].copy()
 
+            # save best local of particle as the best global if it has a better evaluation
             if evaluate_combination(particle_best[particle]) > evaluate_combination(swarm_best):
                 swarm_best = particle_best[particle].copy()
             else:
-                runs_no_improve += 1
+                runs_no_improve += 1 # count movements with no improvement for evaluation
                 
             print("local " + str(particle) + ":", particle_best[particle])
         print("best:", swarm_best)
     print("Final combination:", swarm_best)
-    print("Final evaluation:", evaluate_combination(swarm_best))
+    print("Final evaluation:", evaluate_combination(swarm_best)) # calculate execution time
 
     print('Execution Time: ', time.time() - start_time) # calculate execution time
 
